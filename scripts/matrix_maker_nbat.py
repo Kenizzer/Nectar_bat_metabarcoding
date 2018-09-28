d78f1555d7a18c69913b94fd2d9c3616d1d55ecc
Kenizzer <Kenizzer@gmail.com>
2018-09-28 11:42:45 -0500

    changes to matrix script, increasing readability

--- scripts/matrix_maker_nbat.py
+++ scripts/matrix_maker_nbat.py
@@ -4,36 +4,28 @@ import pandas as pd
 import numpy as np
 import csv
 
-X = glob.glob('*_all-ex.txt')
+file_list = glob.glob('*.txt')
+temp_list = []
 
 with open('taxa_list.csv', 'rb') as file:
-	Wordlist = file.read()
+	taxa_list = file.read()
 
-Wordlist = Wordlist.split(',')
+taxa_list = taxa_list.split(',')
 
-lodf = []
-for t in X:
-	df = pd.read_table(t, header=None, index_col=False, sep='\t')
-	df.columns = ['word', 'count']
-	lodf.append(df)
+for file in file_list:
+	taxa_matrix = pd.read_table(file, header=None, index_col=False, sep='\t')
+	taxa_matrix.columns = ['taxa', 'count']
+	temp_list.append(taxa_matrix)
+
+taxa_matrix = pd.DataFrame()
+taxa_matrix['taxa'] = taxa_list
 
 
-print(len(lodf))
-print(lodf[0].head())
+for i in temp_list:
+	taxa_matrix = taxa_matrix.merge(i, how='left', on='taxa')
 
-#df.to_csv("temp.csv", sep='\t')
+colnames = ['taxa'] + file_list
+taxa_matrix.columns = colnames
 
-df = pd.DataFrame()
-df['word'] = Wordlist
-#print(df.head())
-
-for i in lodf:
-	df = df.merge(i, how='left', on='word')
-
-#print(df.head())
-
-colnames = ['word'] + X
-df.columns = colnames
-
-df = df.transpose()
-df.to_csv('df.csv')
+taxa_matrix = taxa_matrix.transpose()
+taxa_matrix.to_csv('taxa_matrix.csv')
